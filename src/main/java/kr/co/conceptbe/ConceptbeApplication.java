017e88dfa189e8e0b8279091771ef9511923bcc2
parkmuhyeun <pjhg410@gmail.com>
2023-12-19 15:57:45 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/kr/co/conceptbe/ConceptbeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.conceptbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/conceptbe/ConceptbeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.conceptbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
