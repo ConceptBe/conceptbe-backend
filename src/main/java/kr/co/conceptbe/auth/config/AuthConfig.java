a943fde78bfdbb7de1dd545175fbb449864cec30
Mooooooo <pjhg410@gmail.com>
2024-01-03 09:27:40 +0900

    feat: Auth Interceptor, resolver 추가 (#8)
    * feat: Auth Interceptor 추가
    
    * feat: Optional Auth 추가
    
    * chore: interceptor, resolver 설정 추가
    
    * refactor: 토큰 누락 에러 메시지 수정
    
    * fix: AuthInterceptor 잘못된 메서드 수정

--- src/main/java/kr/co/conceptbe/auth/config/AuthConfig.java
+++ src/main/java/kr/co/conceptbe/auth/config/AuthConfig.java
@@ -1,14 +1,26 @@
 package kr.co.conceptbe.auth.config;
 
+import java.util.List;
+import kr.co.conceptbe.auth.presentation.AuthInterceptor;
+import kr.co.conceptbe.auth.presentation.MandatoryJwtArgumentResolver;
 import kr.co.conceptbe.auth.presentation.OauthServerTypeConverter;
+import kr.co.conceptbe.auth.presentation.OptionalJwtArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final MandatoryJwtArgumentResolver mandatoryJwtArgumentResolver;
+    private final OptionalJwtArgumentResolver optionalJwtArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -28,5 +40,16 @@ public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new OauthServerTypeConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(mandatoryJwtArgumentResolver);
+        resolvers.add(optionalJwtArgumentResolver);
     }
 }
---
+++ src/main/java/kr/co/conceptbe/auth/presentation/dto/AuthCredentials.java
@@ -0,0 +1,6 @@
+package kr.co.conceptbe.auth.presentation.dto;
+
+public record AuthCredentials(
+        Long id
+) {
+}
