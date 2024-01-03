fbc3ddc97ee58dd27856cabd6499c240a4f14a74
parkmuhyeun <pjhg410@gmail.com>
2024-01-03 16:03:41 +0900

    test: 상세분류 스킬 조회 테스트

--- src/test/java/kr/co/conceptbe/skill/application/SkillServiceTest.java
+++ src/test/java/kr/co/conceptbe/skill/application/SkillServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import kr.co.conceptbe.skill.application.dto.FindDetailSkillResponse;
 import kr.co.conceptbe.skill.application.dto.FindMainSkillResponse;
 import kr.co.conceptbe.skill.domain.SkillCategory;
 import kr.co.conceptbe.skill.domain.SkillCategoryRepository;
@@ -36,6 +37,23 @@ class SkillServiceTest {
         assertAll(
             () -> assertThat(mainSkills.size()).isEqualTo(1),
             () -> assertThat(mainSkills.get(0).name()).isEqualTo("개발")
+        );
+    }
+
+    @Test
+    void 상세분류_스킬을_조회할_수_있다() {
+        //given
+        SkillCategory mainSkill = skillCategoryRepository.save(new SkillCategory("개발"));
+        mainSkill.updateParentSkill(mainSkill);
+        skillCategoryRepository.save(new SkillCategory(mainSkill, "BE"));
+
+        //when
+        List<FindDetailSkillResponse> detailSkills = skillService.getDetailSkills(mainSkill.getId());
+
+        //then
+        assertAll(
+            () -> assertThat(detailSkills.size()).isEqualTo(1),
+            () -> assertThat(detailSkills.get(0).name()).isEqualTo("BE")
         );
     }
 }
