aa81026dd7d5f87c273f152e15a6a6eccfd9db18
dnd2dnd <tkfdkskarl56@naver.com>
2024-01-09 13:37:57 +0900

    feat : 댓글 validation 추가

--- src/main/java/kr/co/conceptbe/comment/dto/CommentUpdateRequest.java
+++ src/main/java/kr/co/conceptbe/comment/dto/CommentUpdateRequest.java
@@ -1,6 +1,9 @@
 package kr.co.conceptbe.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CommentUpdateRequest (
+	@NotBlank(message = "댓글이 입력되지 않았습니다.")
 	String content
 ) {
 }
