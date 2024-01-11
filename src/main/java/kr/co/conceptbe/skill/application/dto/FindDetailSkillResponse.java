199e901ae5e96af46f9b2d67d3cc2eda7f970766
parkmuhyeun <pjhg410@gmail.com>
2024-01-11 11:19:16 +0900

    refactor: 회원가입 초기 정보 조회 api 추가에 따른 기존 세부 조회 삭제

--- src/main/java/kr/co/conceptbe/skill/application/dto/FindDetailSkillResponse.java
+++
@@ -1,7 +0,0 @@
-package kr.co.conceptbe.skill.application.dto;
-
-public record FindDetailSkillResponse(
-    Long id,
-    String name
-) {
-}
--- src/main/java/kr/co/conceptbe/skill/application/dto/FindMainSkillResponse.java
+++
@@ -1,7 +0,0 @@
-package kr.co.conceptbe.skill.application.dto;
-
-public record FindMainSkillResponse(
-    Long id,
-    String name
-) {
-}
