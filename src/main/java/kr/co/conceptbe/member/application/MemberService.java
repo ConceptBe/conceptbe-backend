63f74737194123de913bd350b3e66d64255b0869
parkmuhyeun <pjhg410@gmail.com>
2024-01-23 12:56:03 +0900

    feat: 프로필 조회 기능 추가

--- src/main/java/kr/co/conceptbe/member/application/MemberService.java
+++ src/main/java/kr/co/conceptbe/member/application/MemberService.java
@@ -1,5 +1,9 @@
 package kr.co.conceptbe.member.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import kr.co.conceptbe.member.application.dto.GetMemberProfileResponse;
+import kr.co.conceptbe.member.domain.Member;
 import kr.co.conceptbe.member.exception.AlreadyExistsNicknameException;
 import kr.co.conceptbe.member.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,5 +21,31 @@ public class MemberService {
         if (memberRepository.existsByNickname(nickname)) {
             throw new AlreadyExistsNicknameException(nickname);
         }
+    }
+
+    public GetMemberProfileResponse getMemberProfileBy(Long id) {
+        Member member = memberRepository.getById(id);
+        return new GetMemberProfileResponse(
+            member.getProfileImageUrl(),
+            member.getNickname(),
+            member.getMainSkill().getName(),
+            member.getWorkingPlace(),
+            member.getWorkingPlace(),
+            member.getIntroduce(),
+            mapToMemberSkills(member),
+            mapToMemberPurposes(member)
+        );
+    }
+
+    private List<String> mapToMemberPurposes(Member member) {
+        return member.getPurposes().stream()
+            .map(purpose -> purpose.getPurpose().getName())
+            .collect(Collectors.toList());
+    }
+
+    private List<String> mapToMemberSkills(Member member) {
+        return member.getSkills().stream()
+            .map(skill -> skill.getSkillCategory().getName())
+            .collect(Collectors.toList());
     }
 }
