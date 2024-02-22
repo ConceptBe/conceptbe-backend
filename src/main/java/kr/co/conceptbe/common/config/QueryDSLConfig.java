05ba4ab53a3aa142cc80ac46f34f168caa5fcb70
jaeyeon kim <jakind@naver.com>
2024-02-23 03:24:05 +0900

    feat: QueryDSL 설정

---
+++ src/main/java/kr/co/conceptbe/common/config/QueryDSLConfig.java
@@ -0,0 +1,18 @@
+package kr.co.conceptbe.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDSLConfig {
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
