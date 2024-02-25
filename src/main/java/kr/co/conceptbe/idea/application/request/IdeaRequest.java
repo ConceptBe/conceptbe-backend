0282672e76946c4578d031312033d7dac85d0be6
jaeyeon kim <jakind@naver.com>
2024-02-25 12:42:09 +0900

    refactor: 배열 Schema, ArraySchema 로 수정

--- src/main/java/kr/co/conceptbe/idea/application/request/IdeaRequest.java
+++ src/main/java/kr/co/conceptbe/idea/application/request/IdeaRequest.java
@@ -1,23 +1,25 @@
 package kr.co.conceptbe.idea.application.request;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record IdeaRequest(
-        @Schema(description = "제목", example = "같이 프로젝트 하실분")
-        String title,
-        @Schema(description = "소개", example = "같이 프로젝트 하실분을 찾습니다.")
-        String introduce,
-        @Schema(description = "협업 방식", example = "온라인")
-        String cooperationWay,
-        @Schema(description = "모집 지역 ID", example = "1")
-        Long recruitmentPlaceId,
-        @Schema(description = "분야 ID 목록", example = "1,2")
-        List<Long> branchIds,
-        @Schema(description = "목적 ID 목록", example = "1,2")
-        List<Long> purposeIds,
-        @Schema(description = "팀원 모집 ID 목록", example = "1,2,3,4")
-        List<Long> skillCategoryIds
+    @Schema(description = "제목", example = "같이 프로젝트 하실분")
+    String title,
+    @Schema(description = "소개", example = "같이 프로젝트 하실분을 찾습니다.")
+    String introduce,
+    @Schema(description = "협업 방식", example = "온라인")
+    String cooperationWay,
+    @Schema(description = "모집 지역 ID", example = "1")
+    Long recruitmentPlaceId,
+    @ArraySchema(arraySchema = @Schema(description = "분야 ID 목록", example = "[\"1\", \"2\"]"))
+    List<Long> branchIds,
+    @ArraySchema(arraySchema = @Schema(description = "목적 ID 목록", example = "[\"1\", \"2\"]"))
+    List<Long> purposeIds,
+    @ArraySchema(arraySchema = @Schema(description = "팀원 ID 목록", example = "[\"1\", \"2\", \"3\", \"4\"]"))
+    List<Long> skillCategoryIds
 ) {
+
 }
 
