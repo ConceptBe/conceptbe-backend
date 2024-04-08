2ba0f8f43ab215efdf597b56a1afa672fd80c80c
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-04-08 21:53:31 +0900

    update : OwnerNotBookmark Errorcode 변경

--- src/main/java/kr/co/conceptbe/bookmark/exception/OwnerNotBookmarkException.java
+++ src/main/java/kr/co/conceptbe/bookmark/exception/OwnerNotBookmarkException.java
@@ -6,5 +6,5 @@ import kr.co.conceptbe.common.exception.ConceptBeException;
 import kr.co.conceptbe.common.exception.ErrorCode;
 
 public class OwnerNotBookmarkException extends ConceptBeException {
-	public OwnerNotBookmarkException() { super(new ErrorCode(HttpStatus.CONFLICT, "본인 글을 스크랩할 수 없습니다.")); }
+	public OwnerNotBookmarkException() { super(new ErrorCode(HttpStatus.BAD_REQUEST, "본인 글을 스크랩할 수 없습니다.")); }
 }
