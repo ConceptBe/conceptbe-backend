aafa1377495a87faccc0df15a3456bccfb2fc50c
jaeyeon kim <jakind@naver.com>
2024-04-12 18:55:23 +0900

    refactor: 반환값 변경

--- src/main/java/kr/co/conceptbe/idea/application/response/MemberResponse.java
+++ src/main/java/kr/co/conceptbe/idea/application/response/MemberResponse.java
@@ -1,10 +1,7 @@
 package kr.co.conceptbe.idea.application.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.List;
-import kr.co.conceptbe.skill.domain.SkillCategory;
 import kr.co.conceptbe.member.domain.Member;
-import kr.co.conceptbe.member.domain.MemberSkillCategory;
 
 public record MemberResponse(
     @Schema(description = "멤버 ID", example = "1")
@@ -13,22 +10,16 @@ public record MemberResponse(
     String profileImageUrl,
     @Schema(description = "닉네임", example = "conceptbe")
     String nickname,
-    @Schema(description = "세부스킬들", example = "BE, 서버개발")
-    List<String> skills
+    @Schema(description = "세부스킬", example = "서버개발")
+    String mainSkill
 ) {
 
     public static MemberResponse from(Member member) {
-        List<String> skills = member.getSkills().getValues()
-            .stream()
-            .map(MemberSkillCategory::getSkillCategory)
-            .map(SkillCategory::getName)
-            .toList();
-
         return new MemberResponse(
             member.getId(),
             member.getProfileImageUrl(),
             member.getNickname(),
-            skills
+            member.getMainSkill().getName()
         );
     }
 
