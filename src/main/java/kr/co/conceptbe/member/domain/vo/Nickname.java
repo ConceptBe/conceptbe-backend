76530fcaed21d47bd6d6be14680197a427b8b4d3
jaeyeon kim <jakind@naver.com>
2024-04-12 20:48:35 +0900

    feat: Nickname VO 구현

---
+++ src/main/java/kr/co/conceptbe/member/domain/vo/Nickname.java
@@ -0,0 +1,49 @@
+package kr.co.conceptbe.member.domain.vo;
+
+import java.util.Objects;
+
+public class Nickname {
+
+    private static final int NICKNAME_LENGTH_LOWER_BOUND_INCLUSIVE = 2;
+    private static final int NICKNAME_LENGTH_UPPER_BOUND_INCLUSIVE = 10;
+    private static final String NICKNAME_PATTERN = "/^[a-zA-Z0-9ㄱ-힣]+$/g";
+    private final String nickname;
+
+    public Nickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public static Nickname from(String nickname) {
+        validateNull(nickname);
+        nickname = nickname.trim();
+        validateLength(nickname);
+        validatePattern(nickname);
+        return new Nickname(nickname);
+    }
+
+    private static void validateNull(String nickname) {
+        if (Objects.nonNull(nickname)) {
+            return;
+        }
+        throw new IllegalArgumentException("닉네임은 필수로 입력하셔야합니다.");
+    }
+
+    private static void validateLength(String nickname) {
+        if (NICKNAME_LENGTH_LOWER_BOUND_INCLUSIVE <= nickname.length()
+            && nickname.length() <= NICKNAME_LENGTH_UPPER_BOUND_INCLUSIVE) {
+            return;
+        }
+        throw new IllegalArgumentException("닉네임은 2글자에서 10글자 사이여야합니다.");
+    }
+
+    private static void validatePattern(String nickname) {
+        if (nickname.matches(NICKNAME_PATTERN)) {
+            return;
+        }
+        throw new IllegalArgumentException("닉네임는 한글/영어/숫자로만 구성되어야 합니다.");
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
