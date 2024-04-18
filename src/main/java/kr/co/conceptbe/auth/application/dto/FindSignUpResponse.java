402c2942753cd02d9591709abcf6a471237a7686
jaeyeon kim <jakind@naver.com>
2024-04-18 15:08:20 +0900

    refactor: FindSignUpResponse에 Region 정보 추

--- src/main/java/kr/co/conceptbe/auth/application/dto/FindSignUpResponse.java
+++ src/main/java/kr/co/conceptbe/auth/application/dto/FindSignUpResponse.java
@@ -2,11 +2,15 @@ package kr.co.conceptbe.auth.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import kr.co.conceptbe.idea.application.response.RegionResponse;
 
 public record FindSignUpResponse(
     @Schema(description = "메인 스킬 리스트")
     List<MainSkillResponse> mainSkillResponses,
     @Schema(description = "가입 목적 리스트")
-    List<PurposeResponse> purposeResponses
+    List<PurposeResponse> purposeResponses,
+    @Schema(description = "가입 지역 리스트")
+    List<RegionResponse> regionResponses
 ) {
+
 }
