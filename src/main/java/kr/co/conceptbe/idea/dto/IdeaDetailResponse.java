14f0227bb53b061a1148d0981ab37d3b77c92962
jaeyeon kim <jakind@naver.com>
2024-04-18 15:24:48 +0900

    refactor: IdeaDetailResponse의 skillList->mainSkill로 변경

--- src/main/java/kr/co/conceptbe/idea/dto/IdeaDetailResponse.java
+++ src/main/java/kr/co/conceptbe/idea/dto/IdeaDetailResponse.java
@@ -2,53 +2,53 @@ package kr.co.conceptbe.idea.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 import kr.co.conceptbe.comment.Comment;
 import kr.co.conceptbe.idea.domain.Idea;
 
-public record IdeaDetailResponse (
-	Long memberId,
-	String imageUrl,
-	String nickname,
-	List<String> skillList,
-	String title,
-	LocalDateTime date,
-	String introduce,
-	List<String> branchList,
-	List<String> purposeList,
-	String cooperationWay,
-	String recruitmentPlace,
-	List<String> skillCategories,
-	Integer likesCount,
-	Integer commentsCount,
-	Integer bookmarksCount,
-	Integer hits,
-	Boolean owner,
-	Boolean ownerLike,
-	Boolean ownerScrap
+public record IdeaDetailResponse(
+    Long memberId,
+    String imageUrl,
+    String nickname,
+    String mainSkill,
+    String title,
+    LocalDateTime date,
+    String introduce,
+    List<String> branchList,
+    List<String> purposeList,
+    String cooperationWay,
+    String recruitmentPlace,
+    List<String> skillCategories,
+    Integer likesCount,
+    Integer commentsCount,
+    Integer bookmarksCount,
+    Integer hits,
+    Boolean owner,
+    Boolean ownerLike,
+    Boolean ownerScrap
 ) {
-	public static IdeaDetailResponse of(Long tokenMemberId, Idea idea) {
-		return new IdeaDetailResponse(
-			idea.getCreator().getId(),
-			idea.getCreator().getProfileImageUrl(),
-			idea.getCreator().getNickname(),
-			idea.getCreator().getSkills().getValues().stream().map(e -> e.getSkillCategory().getName()).toList(),
-			idea.getTitle(),
-			idea.getCreatedAt(),
-			idea.getIntroduce(),
-			idea.getBranches().stream().map(e -> e.getBranch().getName()).toList(),
-			idea.getPurposes().stream().map(e -> e.getPurpose().getName()).toList(),
-			idea.getCooperationWay(),
-			idea.getRecruitmentPlace(),
-			idea.getSkillCategories().stream().map(e -> e.getSkillCategory().getName()).toList(),
-			idea.getLikesCount(),
-			idea.getComments().stream().filter(Comment::isParentComment)
-				.mapToInt(e -> e.getCommentsCount() + 1).sum(),
-			idea.getBookmarksCount(),
-			idea.getHitsCount(),
-			idea.isOwner(tokenMemberId),
-			idea.isOwnerLike(tokenMemberId),
-			idea.isOwnerScrap(tokenMemberId)
-		);
-	}
+
+    public static IdeaDetailResponse of(Long tokenMemberId, Idea idea) {
+        return new IdeaDetailResponse(
+            idea.getCreator().getId(),
+            idea.getCreator().getProfileImageUrl(),
+            idea.getCreator().getNickname(),
+            idea.getCreator().getMainSkill().getName(),
+            idea.getTitle(),
+            idea.getCreatedAt(),
+            idea.getIntroduce(),
+            idea.getBranches().stream().map(e -> e.getBranch().getName()).toList(),
+            idea.getPurposes().stream().map(e -> e.getPurpose().getName()).toList(),
+            idea.getCooperationWay(),
+            idea.getRecruitmentPlace(),
+            idea.getSkillCategories().stream().map(e -> e.getSkillCategory().getName()).toList(),
+            idea.getLikesCount(),
+            idea.getComments().stream().filter(Comment::isParentComment)
+                .mapToInt(e -> e.getCommentsCount() + 1).sum(),
+            idea.getBookmarksCount(),
+            idea.getHitsCount(),
+            idea.isOwner(tokenMemberId),
+            idea.isOwnerLike(tokenMemberId),
+            idea.isOwnerScrap(tokenMemberId)
+        );
+    }
 }
