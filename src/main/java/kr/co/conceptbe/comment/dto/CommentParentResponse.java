300c0b4e99b17dee49961a939f4f42684d6753f4
jaeyeon kim <jakind@naver.com>
2024-04-18 15:26:34 +0900

    refactor: Comment SkillList->mainSkill로 변경

--- src/main/java/kr/co/conceptbe/comment/dto/CommentParentResponse.java
+++ src/main/java/kr/co/conceptbe/comment/dto/CommentParentResponse.java
@@ -2,41 +2,41 @@ package kr.co.conceptbe.comment.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 import kr.co.conceptbe.comment.Comment;
 
-public record CommentParentResponse (
-	Long memberId,
-	Long parentCommentId,
-	String nickname,
-	String profileImageUrl,
-	List<String> memberSkills,
-	LocalDateTime createdAt,
-	String content,
-	Integer likesCount,
-	Integer commentCount,
-	List<CommentChildResponse> commentChildResponses,
-	Boolean owner,
-	Boolean deleted,
-	Boolean likes
+public record CommentParentResponse(
+    Long memberId,
+    Long parentCommentId,
+    String nickname,
+    String profileImageUrl,
+    String memberMainSkill,
+    LocalDateTime createdAt,
+    String content,
+    Integer likesCount,
+    Integer commentCount,
+    List<CommentChildResponse> commentChildResponses,
+    Boolean owner,
+    Boolean deleted,
+    Boolean likes
 ) {
-	public static CommentParentResponse of(Comment comment, Long tokenMemberId) {
-		return new CommentParentResponse(
-			comment.getCreator().getId(),
-			comment.getId(),
-			comment.getCreator().getNickname(),
-			comment.getCreator().getProfileImageUrl(),
-			comment.getCreator().getSkills().getValues().stream().map(e -> e.getSkillCategory().getName()).toList(),
-			comment.getCreatedAt(),
-			comment.getContent(),
-			comment.getLikesCount(),
-			comment.getCommentsCount(),
-			comment.getComments()
-				.stream()
-				.map(commentParent -> CommentChildResponse.of(commentParent, tokenMemberId))
-				.toList(),
-			comment.isOwner(tokenMemberId),
-			comment.getDeleted(),
-			comment.isLike(tokenMemberId));
-	}
+
+    public static CommentParentResponse of(Comment comment, Long tokenMemberId) {
+        return new CommentParentResponse(
+            comment.getCreator().getId(),
+            comment.getId(),
+            comment.getCreator().getNickname(),
+            comment.getCreator().getProfileImageUrl(),
+            comment.getCreator().getMainSkill().getName(),
+            comment.getCreatedAt(),
+            comment.getContent(),
+            comment.getLikesCount(),
+            comment.getCommentsCount(),
+            comment.getComments()
+                .stream()
+                .map(commentParent -> CommentChildResponse.of(commentParent, tokenMemberId))
+                .toList(),
+            comment.isOwner(tokenMemberId),
+            comment.getDeleted(),
+            comment.isLike(tokenMemberId));
+    }
 }
