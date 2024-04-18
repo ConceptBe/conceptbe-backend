b5cf0bbea3ae856a96226948720617da2da2a879
jaeyeon kim <jakind@naver.com>
2024-04-18 14:57:43 +0900

    refactor: UTC+9 로 설정

--- src/main/java/kr/co/conceptbe/ConceptbeApplication.java
+++ src/main/java/kr/co/conceptbe/ConceptbeApplication.java
@@ -1,5 +1,7 @@
 package kr.co.conceptbe;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -10,7 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class ConceptbeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConceptbeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConceptbeApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
