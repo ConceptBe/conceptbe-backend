9e8670b2c029c7505e4f16e6f9ebbb8b16e3c119
jaeyeon kim <jakind@naver.com>
2024-06-12 19:38:56 +0900

    feat: image upload 기능 구현

--- src/main/java/kr/co/conceptbe/image/application/ImageService.java
+++ src/main/java/kr/co/conceptbe/image/application/ImageService.java
@@ -6,7 +6,11 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
+import kr.co.conceptbe.image.domain.IdeaValidator;
+import kr.co.conceptbe.image.domain.Image;
+import kr.co.conceptbe.image.domain.ImageRepository;
 import kr.co.conceptbe.image.domain.UploadFile;
+import kr.co.conceptbe.image.exception.IdeaNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,31 +20,54 @@ public class ImageService {
 
     @Value("${s3.bucket}")
     private String bucket;
-    @Value("${cloud-front-url}")
-    private String cloudFrontUrl;
+//    @Value("${cloud-front-url}")
+//    private String cloudFrontUrl;
     private final AmazonS3 amazonS3;
+    private final ImageRepository imageRepository;
+    private final IdeaValidator ideaValidator;
 
-    public ImageService(AmazonS3 amazonS3) {
+    public ImageService(
+        AmazonS3 amazonS3,
+        ImageRepository imageRepository,
+        IdeaValidator ideaValidator
+    ) {
         this.amazonS3 = amazonS3;
+        this.imageRepository = imageRepository;
+        this.ideaValidator = ideaValidator;
+    }
+
+    public Long save(Long ideaId, MultipartFile multipartFile) {
+        validateIdea(ideaId);
+        String imageUrl = upload(multipartFile);
+        Image image = new Image(ideaId, imageUrl);
+        Image savedImage = imageRepository.save(image);
+        return savedImage.getId();
+    }
+
+    private void validateIdea(Long ideaId) {
+        if (ideaValidator.existsIdea(ideaId)) {
+            return;
+        }
+        throw new IdeaNotFoundException();
     }
 
     private String upload(MultipartFile multipartFile) {
         try {
             UploadFile uploadFile = UploadFile.from(multipartFile);
             fileUpload(uploadFile);
-            return getUploadPath(uploadFile);
+            return uploadFile.getOriginalFilename();
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
     }
 
-    private String getUploadPath(UploadFile uploadFile) {
-        return String.join(
-            "/",
-            cloudFrontUrl,
-            uploadFile.getOriginalFilename()
-        );
-    }
+//    private String getUploadPath(UploadFile uploadFile) {
+//        return String.join(
+//            "/",
+//            cloudFrontUrl,
+//            uploadFile.getOriginalFilename()
+//        );
+//    }
 
 
     private void fileUpload(MultipartFile multipartFile) throws IOException {
