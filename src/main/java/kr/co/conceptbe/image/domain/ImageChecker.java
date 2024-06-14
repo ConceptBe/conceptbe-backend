8d906bab165a5a240074f96567d9835a5941c03d
jaeyeon kim <jakind@naver.com>
2024-06-14 16:56:38 +0900

    feat: ImageChecker 구현

---
+++ src/main/java/kr/co/conceptbe/image/domain/ImageChecker.java
@@ -0,0 +1,33 @@
+package kr.co.conceptbe.image.domain;
+
+import java.util.List;
+import kr.co.conceptbe.image.exception.ExceedImageSizeException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImageChecker {
+
+    private static final int IMAGE_SIZE_UPPER_BOUND_INCLUSIVE = 3;
+
+    public List<Long> imageIdsToDeleted(List<Long> originalImageIds, List<Long> changedImageIds) {
+        return originalImageIds.stream()
+            .filter(id -> !changedImageIds.contains(id))
+            .toList();
+    }
+
+    public void validateTotalImageSize(
+        int originalImageSize,
+        int deletedImageSize,
+        int additionImageSize
+    ) {
+        int imageSizeAfterDeleted = originalImageSize - deletedImageSize;
+        int imageSizeAfterUpdate = imageSizeAfterDeleted + additionImageSize;
+
+        if (imageSizeAfterUpdate <= IMAGE_SIZE_UPPER_BOUND_INCLUSIVE) {
+            return;
+        }
+
+        throw new ExceedImageSizeException(IMAGE_SIZE_UPPER_BOUND_INCLUSIVE);
+    }
+
+}
